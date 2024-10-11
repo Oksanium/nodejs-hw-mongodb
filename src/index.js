615930f3782e461eca2c273935be6580c370e5a9
Oks <witchox@gmail.com>
2024-10-11 21:00:30 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoDB } from './db/initMongoConnection.js';
 import { startServer } from './server.js';
 
 const bootstrap = async () => {
