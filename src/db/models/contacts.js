16d496cee33c053119f3344c2d097b7fb8fb5367
Oks <witchox@gmail.com>
2024-11-13 06:29:30 +0100

    done

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -19,7 +19,6 @@ const contactsSchema = new Schema(
     },
     contactType: {
       type: String,
-      required: true,
       enum: ['work', 'home', 'personal'],
       default: 'personal',
     },
--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import { env } from './utils/env.js';
 import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { ErrorHandler } from './middlewares/errorHandler.js';
-
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -36,6 +35,8 @@ export function startServer() {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
